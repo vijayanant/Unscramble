dc94167599affa844b7f1baec51efeb267d4c410
Vijay Ananth <vijay.hassan@gmail.com>
2014-12-06 14:06:05 +0530

    Fixed major bug in get()

--- src/com/vj/TestClient.java
+++ src/com/vj/TestClient.java
@@ -6,6 +6,8 @@ public class TestClient {
     public static void main(String[] args) {
 
         Unscramble ds = new Unscramble(args[0]);
+        System.out.println("ds.size = " + ds.size());
+        System.out.println("NODE_COUNT = " + ds.size());
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("On each line provide <letters> <space> <key character>\n\n");
@@ -14,6 +16,7 @@ public class TestClient {
             char keyLetter = scanner.next().trim().charAt(0);
             Iterable<String> words = ds.get(letters, keyLetter);
             System.out.println("get(" + letters + ", " + keyLetter + ") = " + words);
+//            System.out.println("Get call count = " + ds.GET_CALL_COUNT);
         }
     }
 
--- src/com/vj/Unscramble.java
+++ src/com/vj/Unscramble.java
@@ -10,11 +10,17 @@ public final class Unscramble {
     private Node root;
     private static final String LETTERS = "abcdefghijklmnopqrstuvwxyz";
     private static final int NUMBER_OF_LETTERS = LETTERS.length();
+    private static int NODE_COUNT = 0;
+    public static int GET_CALL_COUNT = 0;
 
     private class Node {
         ArrayList<String> value;
         Character key;
         Node yes, no;
+
+        public Node () {
+            NODE_COUNT++;
+        }
     }
 
     public Unscramble() {
@@ -38,7 +44,13 @@ public final class Unscramble {
         return root == null;
     }
 
+    public int size() {
+        return NODE_COUNT;
+    }
+
+
     public final Iterable<String> get(String key, char keyChar) {
+        GET_CALL_COUNT = 0;
         char[] chars = key.toLowerCase().toCharArray();
         Arrays.sort(chars);
         int keyCharPos = LETTERS.indexOf(keyChar);
@@ -48,6 +60,7 @@ public final class Unscramble {
     }
 
     public final Iterable<String> get(String key) {
+        GET_CALL_COUNT = 0;
         char[] chars = key.toLowerCase().toCharArray();
         Arrays.sort(chars);
         ArrayList<String> result = new ArrayList<String>();
@@ -56,12 +69,11 @@ public final class Unscramble {
     }
 
     private final void get(Node x, String key, int index, int depth, int keyCharPos, ArrayList<String> result) {
-        //System.out.println("GET x.key = [" + x.key + "], key = [" + key + "], index = [" + index + "], depth = [" + depth + "], keyCharPos = [" + keyCharPos + "], result = [" + result + "]");
         if (x == null) return;
+        GET_CALL_COUNT++;
+//        System.out.println("GET x.key = [" + x.key + "], key = [" + key + "], index = [" + index + "], depth = [" + depth + "], keyCharPos = [" + keyCharPos + "], result = [" + result + "]");
 
         char c = key.charAt(index);
-
-
         if (keyCharPos != -1 &&  // there is a key char, all strings must include this char
             depth == keyCharPos &&  // this is the key char
             c != LETTERS.charAt(keyCharPos)) { //this char must be present, otherwise return
@@ -69,30 +81,31 @@ public final class Unscramble {
                 return;
         }
 
-
         //ignore repetitive chars
         while (index < key.length() - 1 && c == key.charAt(index + 1))
             c = key.charAt(++index);
 
-        if (c == x.key) {  //yes path
+        if (c == x.key) {
             if (x.value != null && depth >= keyCharPos) result.addAll(x.value);
-            if (index != key.length() - 1)    // No more chars to match
-                get(x.yes, key, ++index, ++depth, keyCharPos, result); // traverse the yes path
+            if (index != key.length() - 1) {   // more chars to match
+                // Travese both ways
+                get(x.yes, key, index + 1, depth + 1, keyCharPos, result); // traverse the 'yes' path
+                get(x.no, key, index+1, depth + 1, keyCharPos, result); //traverse the 'no' path as well
+            }
         } else {//no path, don't progress the key index
-            get(x.no, key, index, ++depth, keyCharPos, result);
+            get(x.no, key, index, depth+1, keyCharPos, result);
         }
     }
 
     public final void put(String key) {
         char[] chars = key.toLowerCase().toCharArray();
         Arrays.sort(chars);
-
         root = put(root, key, String.copyValueOf(chars), 0, 0);
     }
 
     private final Node put(Node x, String key, String sortedKey, int index, int depth) {
         if (depth == NUMBER_OF_LETTERS) depth--;
-        //System.out.println("PUT x.key = [" + LETTERS.charAt(depth) + "], sortedKey = [" + sortedKey + "], index = [" + index + "], depth = [" + depth + "]");
+//        System.out.println("PUT x.key = [" + LETTERS.charAt(depth) + "], sortedKey = [" + sortedKey + "], index = [" + index + "], depth = [" + depth + "]");
         if (x == null) {
             x = new Node();
             x.key = LETTERS.charAt(depth);
@@ -103,7 +116,6 @@ public final class Unscramble {
         while (index < sortedKey.length() - 1 && c == sortedKey.charAt(index + 1))
             c = sortedKey.charAt(++index);
 
-
         if (c > LETTERS.charAt(depth))  //No path
             x.no = put(x.no, key, sortedKey, index, ++depth);
 
@@ -112,16 +124,21 @@ public final class Unscramble {
         else {
             if (x.value == null) x.value = new ArrayList<String>();
             x.value.add(key);
-            //System.out.println("x.key = " + x.key + ", word = " + key);
+//            System.out.println("x.key = " + x.key + ", word = " + key);
         }
-
         return x;
     }
 
+
     public static void main(String[] args) {
         Unscramble ds = new Unscramble();
-        final String s = "emerald";
-        ds.put(s);
-        System.out.println(ds.get("merald", 'e'));
+
+        ds.put("blink");
+        ds.put("blinker");
+        ds.put("abc");
+        ds.put("ac");
+
+        System.out.println(ds.get("blinker", 'b'));
+//        System.out.println("ds.size = " + ds.size());
     }
 }
