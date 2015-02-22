47d11cf583363a1abaedba474a1a1b3a70f4b2a9
Vijay Ananth <vijay.hassan@gmail.com>
2015-02-22 18:50:19 +0530

    fixed  minor error in client

--- src/com/vj/TestClient.java
+++ src/com/vj/TestClient.java
@@ -1,5 +1,6 @@
 package com.vj;
 
+import java.util.Collection;
 import java.util.Scanner;
 
 public class TestClient {
@@ -14,11 +15,28 @@ public class TestClient {
         Scanner scanner = new Scanner(System.in);
         System.out.println("On each line provide <letters> <space> <key character>\n\n");
         while (scanner.hasNextLine()) {
-            String letters = scanner.next();
-            char keyLetter = scanner.next().trim().charAt(0);
-            Iterable<String> words = ds.get(letters, keyLetter);
-            System.out.println("get(" + letters + ", " + keyLetter + ") = " + words);
+            Iterable<String> words;
+            String letters;
+            char keyLetter;
+
+            String[] l = scanner.nextLine().split(" ");
+            letters = l[0];
+            if (l.length == 2) {
+                keyLetter = l[1].charAt(0);
+                words = ds.get(letters, keyLetter);
+                System.out.print("get(" + letters + ", '" + keyLetter + "') = [ ");
+            } else {
+                words = ds.get(letters);
+                System.out.print("get(" + letters + "') = [ ");
+            }
+            for (String w : words) {
+                if (w.length()>=4)
+                    System.out.print(w + ", ");
+            }
+            System.out.println("]");
+
             System.out.println("Get call count = " + ds.GET_NODES_VISITED);
+            System.out.println("Matching words = " + ((Collection<?>)words).size());
         }
     }
 
